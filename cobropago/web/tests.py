cdd37696840598192a688a53bb7b3cf2937e5744
Luis Fernando Barrera <luisfernando@informind.com>
2016-08-07 05:51:50 -0500

    First Tests

--- cobropago/web/tests.py
+++ cobropago/web/tests.py
@@ -1,3 +1,16 @@
+from django.core.urlresolvers import reverse
 from django.test import TestCase
+from django.http import HttpRequest
+from django.test.client import RequestFactory
+from web.views import HomePageView
 
-# Create your tests here.
+
+class HomePageTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_resolvepage(self):
+        request = self.factory.get('/')
+        response = HomePageView.as_view()(request)
+        self.assertEqual(response.status_code, 200)
+
