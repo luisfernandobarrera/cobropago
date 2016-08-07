296e59f92d8aa6fc4b71589225b839fcce43a63b
Luis Fernando Barrera <luisfernando@informind.com>
2016-08-07 03:26:39 -0500

    Tests & Pycharm

--- cobropago/config/__init__.py
+++ cobropago/config/__init__.py
@@ -3,4 +3,4 @@ from __future__ import absolute_import
 from .local import Local  # noqa
 from .production import Production  # noqa
 from .test import Test  # noqa
-from .test import Test  # noqa
+from .citest import CITest  # noqa
