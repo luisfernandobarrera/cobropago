ec77d4a4016468e4ee02cef32827266578a153ba
Luis Fernando Barrera <luisfernando@informind.com>
2016-08-21 21:18:55 -0500

    Login

--- frontend/components/layout.js
+++ frontend/components/layout.js
@@ -4,9 +4,13 @@ import Sidebar from './sidebar';
 export default class Layout extends React.Component {
     render() {
         return (
-            <div className="layout">
-                <Sidebar/>
+            <div className="app">
+                <div className="container-fluid">
+                    <div className="layout row">
+                        <Sidebar/>
+                    </div>
+                </div>
             </div>
-        )
+    )
     }
-}
+    }
