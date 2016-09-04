bdb4c172ef3bd2bbdbf436682c655d75a23dd792
Luis Fernando Barrera <luisfernando@informind.com>
2016-09-04 18:43:59 -0500

    Add Ledger

--- frontend/containers/ledgers/index.js
+++ frontend/containers/ledgers/index.js
@@ -3,25 +3,50 @@ import LedgerList from '../../components/ledgers';
 import {bindActionCreators} from 'redux';
 import {actions} from '../../resources/ledger';
 import {connect} from 'react-redux';
+import {reset} from 'redux-form';
+import {NewLedgerForm} from './forms'
+
 
 
 class LedgerView extends React.Component {
-  componentWillUpdate() {
-    this.props.actions.fetchLedgers();
+  constructor(props) {
+    super(props);
+    this.create = this.create.bind(this);
+  }
+
+  componentDidMount() {
+    if (this.props.login.get('loggedIn')) {
+      this.props.actions.fetchLedgers();
+    }
+  }
+
+  create(values) {
+    this.props.actions.createLedger(values).then(() => {
+      this.props.actions.fetchLedgers();
+      this.context.store.dispatch(reset('AddLedger'));
+    });
   }
 
   render() {
     let results = this.props.state.items.results || [];
     return (
       <div>
+        <NewLedgerForm onSubmit={this.create} />
         <LedgerList ledgers={results} />
       </div>
     );
   }
 }
 
+
+LedgerView.contextTypes = {
+  store: React.PropTypes.object.isRequired
+};
+
+
 export default connect(
   (state)=>({state: state.ledgers, login: state.login}),
   (dispatch)=>({actions: bindActionCreators(actions, dispatch)})
 )(LedgerView);
+
 
--- frontend/resources/index.js
+++ frontend/resources/index.js
@@ -3,5 +3,5 @@ import {defaultHeaders} from 'redux-rest-resource';
 
 export const token = localStorage.getItem('token');
 export const apiHome = 'http://localhost:8000/api/v1';
-export const apiMe = apiHome + 'users/me';
+export const apiMe = apiHome + '/users/me';
 Object.assign(defaultHeaders, {Authorization: `Token ${token}`});
