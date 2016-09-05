a327bf10401b56f7c21ff745b2123811ef3fed99
Luis Fernando Barrera <luisfernando@informind.com>
2016-09-04 22:45:30 -0500

    URL Home

--- frontend/resources/index.js
+++ frontend/resources/index.js
@@ -2,6 +2,7 @@ import {defaultHeaders} from 'redux-rest-resource';
 
 
 export const token = localStorage.getItem('token');
-export const apiHome = 'http://localhost:8000/api/v1';
+export const origin = window.location.origin;
+export const apiHome = origin + '/api/v1';
 export const apiMe = apiHome + '/users/me';
 Object.assign(defaultHeaders, {Authorization: `Token ${token}`});
