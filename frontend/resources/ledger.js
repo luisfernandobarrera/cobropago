8911e3f9fbed614c37810ef39b5fe215eca4e310
Luis Fernando Barrera <luisfernando@informind.com>
2016-09-17 21:18:00 -0500

    Bug autenticación

--- frontend/resources/ledger.js
+++ frontend/resources/ledger.js
@@ -1,7 +1,14 @@
-import {createResource} from 'redux-rest-resource';
+import {createResource, defaultHeaders} from 'redux-rest-resource';
 import {apiHome} from './index'
+
+let token = localStorage.getItem('token');
+
+if (!!token) {
+  Object.assign(defaultHeaders, {Authorization: `Token ${token}`});
+}
+
 
 export const {types, actions, reducers} = createResource({
   name: 'ledger',
-  url: `${apiHome}/ledgers/:id`
+  url: `${apiHome}/ledgers/:id`,
 });
