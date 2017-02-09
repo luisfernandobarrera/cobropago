c43f3bfff81de91611d5a371f3b9ccb5a35912f4
Luis Fernando Barrera <luisfernando@informind.com>
2017-02-09 01:38:34 -0600

    Login

--- frontend/resources/index.js
+++
@@ -1,5 +0,0 @@
-export const token = localStorage.getItem('token');
-export const origin = window.location.origin;
-export const apiHome = origin + '/api/v1';
-export const apiMe = apiHome + '/users/me';
-
